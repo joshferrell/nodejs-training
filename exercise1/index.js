863b15cbd8ff23e3f61abf381acf7b485d1043ae
Joshua Ferrell <jferrell@wha-mbp5.local>
2017-02-17 11:55:56 -0800

    feat: read file async

--- exercise1/index.js
+++ exercise1/index.js
@@ -1,33 +1,35 @@
-var fs = require('fs');
-var assert = require('assert');
+const fs = require('fs');
+const assert = require('assert');
 
-var f = fs.readFileSync('plans.csv', 'utf8');
+fs.readFile('plans.csv', 'utf8', function ( err, data ) {
+    if (err) return -1;
 
-console.log(f);
+    var plans = [];
+    const lines = data.split('\n');
 
-var plans = [];
-var lines = f.split('\n');
-
-plans = lines
-    .filter(line => line !== "")
-    .map(line => {
-        let tmp = line.split(',');
-        return Object.assign({}, {
-            planName: tmp[0],
-            planBenefits: tmp[1],
-            group: tmp[2]
+    plans = lines
+        .filter(line => line !== "")
+        .map(line => {
+            let tmp = line.split(',');
+            return Object.assign({}, {
+                planName: tmp[0],
+                planBenefits: tmp[1],
+                group: tmp[2]
+            });
         });
-    });
 
-assert.deepEqual(plans,
-    [{
-        planName: 'Great HMO',
-        planBenefits: 'benefits package 1',
-        group: 'UCD'
-    },
-    {
-        planName: 'Silver PPO',
-        planBenefits: 'benefits package 2',
-        group: 'WHA'
-    }]
-);
+    console.log(plans);
+
+    assert.deepEqual(plans,
+        [{
+            planName: 'Great HMO',
+            planBenefits: 'benefits package 1',
+            group: 'UCD'
+        },
+        {
+            planName: 'Silver PPO',
+            planBenefits: 'benefits package 2',
+            group: 'WHA'
+        }]
+    );
+});
